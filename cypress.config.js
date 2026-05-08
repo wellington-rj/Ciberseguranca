422a42b75425225746c250dbd5ef41925a7465af
wellington-rj <wellingtonribeirojardim123@gmail.com>
2026-05-08 08:54:44 -0300

    configurando base url

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
   pageLoadTimeout: 120000,
-  baseUrl: "https://concientizacaociber.netlify.app",
 
   e2e: {
+    baseUrl: "https://concientizacaociber.netlify.app",
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {},
   },
